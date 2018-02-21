3f7c220a47c2e83b5951c0d9caffc6c34fd57590
shpakweb <34437417+shpakweb@users.noreply.github.com>
2018-02-21 19:20:15 +0200

    media footer

--- pilipenko/js/common.js
+++ pilipenko/js/common.js
@@ -21,38 +21,8 @@ $(document).ready(function() {
 		$(".main_mnu ul").slideToggle();
 
 	});
-	/*change logotip */
+
 	
-	
-
-
-	//Таймер обратного отсчета
-	//Документация: http://keith-wood.name/countdown.html
-	//<div class="countdown" date-time="2015-01-07"></div>
-	var austDay = new Date($(".countdown").attr("date-time"));
-	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
-
-	//Попап менеджер FancyBox
-	//Документация: http://fancybox.net/howto
-	//<a class="fancybox"><img src="image.jpg" /></a>
-	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
-	$(".fancybox").fancybox();
-
-	//Навигация по Landing Page
-	//$(".top_mnu") - это верхняя панель со ссылками.
-	//Ссылки вида <a href="#contacts">Контакты</a>
-	$(".top_mnu").navigation();
-
-	//Добавляет классы дочерним блокам .block для анимации
-	//Документация: http://imakewebthings.com/jquery-waypoints/
-	$(".block").waypoint(function(direction) {
-		if (direction === "down") {
-			$(".class").addClass("active");
-		} else if (direction === "up") {
-			$(".class").removeClass("deactive");
-		};
-	}, {offset: 100});
-
 	//Плавный скролл до блока .div по клику на .scroll
 	//Документация: https://github.com/flesler/jquery.scrollTo
 	$("a.scroll").click(function() {
@@ -75,18 +45,3 @@ $(document).ready(function() {
 	
 
 });
-
-//<!-- =============================================== -->
-//<!-- ========== Navbar change logotip========== -->
-//<!-- =============================================== -->
- 
-
-/*
-window.onscroll = function() {
-  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  document.getElementById('home').style.backgroundColor= scrolled==0?"red":"blue";
-}   */
-
-
-
-
