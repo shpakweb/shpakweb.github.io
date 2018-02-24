5087e46b027aeb48017efaedd84afada114a816d
shpakweb <shpakweb@gmail.com>
2018-02-24 18:05:52 +0200

    added node modules and fixed some styles for adaptivity and crossbrausing

--- freelancer/src/js/common.js
+++ freelancer/src/js/common.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
 
-	$(".auth_buttons").click(function(){
-		$(this).next().slideToggle();
-	}	)
 	$(".main_mnu_button").click(function(){
-		$(".main_mnu").slideToggle();
-
-	}	)
+		$(".main_mnu_button").toggleClass("opened");
+		if($(".main_mnu").hasClass("opened")){
+			$(".main_mnu").removeClass('fadeInRight').addClass('fadeOutRight');
+		}
+		else{
+			$(".main_mnu").removeClass('fadeOutRight').addClass('animated fadeInRight');
+		}
+		$(".main_mnu").toggleClass("opened")
+	
+	})
 
 	$("a.scroll").click(function() {
 		$.scrollTo($(".div"), 800, {
@@ -23,7 +27,15 @@ $(document).ready(function() {
 		}, 800);
 		return false;
 	});
-	
+
+	window.onscroll = function() {
+	  if(window.pageYOffset > 750 ) {
+	  	$(".heading").addClass('colored_mnu');
+	  }
+	  else {
+	  	$(".heading").removeClass('colored_mnu');
+	  }
+	}
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
 	$("form").submit(function() {
